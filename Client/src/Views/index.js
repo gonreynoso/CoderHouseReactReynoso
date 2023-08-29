9a49a44edd9270a6aa1edad05245b13716a92056
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2023-08-29 18:36:48 -0300

    fix container item y css general

---
+++ Client/src/Views/index.js
@@ -0,0 +1,7 @@
+import Detail from "./Detail/detail";
+import Home from "./Home/home";
+import About from "./About/about";
+import NotFound from "./NotFound/NotFound";
+import Contact from "./Contact/Contact";
+
+export { Detail, Home, About, NotFound, Contact};
