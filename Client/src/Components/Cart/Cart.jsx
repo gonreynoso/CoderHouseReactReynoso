753d23535a3c6c5796f71faade8abfb2d9dfde66
Gonzalo Reynoso <gonzalo.reynoso9@gmail.com>
2023-09-26 18:13:37 -0300

    actualizando cart

--- Client/src/Components/Cart/Cart.jsx
+++ Client/src/Components/Cart/Cart.jsx
@@ -6,59 +6,55 @@ import styles from './Cart.module.css';
 
 
 const Cart = () => {
-    const { cart, removeItem, clearCart } = useContext(CartContext);
+  const { cart, removeItem, clearCart } = useContext(CartContext);
 
-    const isEmpty = cart.length === 0;
+  const isEmpty = cart.length === 0;
 
-    const calculateTotal = () => {
-        let total = 0;
-        cart.forEach((item) => {
-            total += item.price * item.quantity;
-        });
-        return total;
-    };
+  const calculateTotal = () => {
+    let total = 0;
+    cart.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    return total;
+  };
 
-    return (
+  return (
+    <>
+     <h1 className={styles['h1-cart']}>Carrito</h1>
+    <div className={styles.container}>
+      {isEmpty ? (
         <div>
-            <h2 className={styles['h2-cart']}>Tu carrito</h2>
-            {isEmpty ? (
-                <div>
-                    <p className={styles['empty-cart-p']}>Tu carrito está vacio!</p>
-                    <Link to="/"><button className={styles['empty-cart-btn']}> Ver productos  </button></Link>
-                </div>
-            ) : (
-                <div>
-                    <ul>
-                        {cart.map((item) => (
-                            <li key={item.id} className={styles['li-cart']}>
-                                <div>
-                                    <img className={styles['img-cart']} src={`/img/${item.imageId}`} alt={item.title} />
-                                </div>
-                                <div>
-                                    <h3 className={styles['name-cart']}>{item.title}</h3>
-                                </div>
-                                <div className={styles['info-cart']}>
-                                    <p className={styles['price-cart']}>Precio: ${item.price}</p>
-                                    <p className={styles['cant-cart']}>Cantidad: {item.quantity}</p>
-                                    <FaTrash className={styles['delete-icon']} onClick={() => removeItem(item.id)} />
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                    <div>
-                        <div className={styles['cart-total']}>
-                            <p>Total: ${calculateTotal()}</p>
-                        </div>
-                        <button className={styles['vaciar-cart']} onClick={() => clearCart()}>Vaciar carrito</button>
-                        <Link to="/checkout">
-                            <button className={styles['comprar-cart']}>Realizar compra</button>
-                        </Link>
-                        <Link to="/"><button className={styles['ver-mas-cart']}> Ver mas productos  </button></Link>
-                    </div>
-                </div>
-            )}
+          <p className={styles['empty-cart-p']}>Tu carrito está vacio!</p>
+          <Link to="/"><button className={styles['empty-cart-btn']}> Ver productos  </button></Link>
         </div>
-    );
+      ) : (
+        <div className={styles.container}>
+          <ul className={styles.ul}>
+            {cart.map((item) => (
+              <li key={item.id} className={styles['li-cart']}>
+                <div className={styles.container}>
+                  <h3 className={styles['name-cart']}>{item.title}</h3>
+                  <img className={styles['img-cart']} src={`/img/${item.imageId}`} alt={item.title} />
+                  <p className={styles['price-cart']}>Precio: ${item.price}</p>
+                  <p className={styles['cant-cart']}>Cantidad: {item.quantity}</p>
+                  <FaTrash className={styles['delete-icon']} onClick={() => removeItem(item.id)} />
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className={styles['cart-container']}>
+              <p className={styles['cart-p']}>Total: ${calculateTotal()}</p>
+              <button className={styles['vaciar-cart']} onClick={() => clearCart()}>Vaciar carrito</button>
+              <Link to="/checkout">
+                <button className={styles['comprar-cart']}>Realizar compra</button>
+              </Link>
+            </div>
+        </div>
+      )}
+    </div>
+    </>
+     
+  );
 };
 
 export default Cart;
